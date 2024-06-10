e190df658ed27ba4290eb06ac0d2e02e6d1419cc
smetanin_student <st078182@student.spbu.ru>
2024-06-10 08:36:37 +0000

    some changes in app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask​
+from flask import Flask
 
-app = Flask(__name__)​
+app = Flask(__name__)
 
-
-
-@app.route("/admin")​
-def hello_admin():​
-    return "Hello Admin"​
+@app.route("/admin")
+def hello_admin():
+    return "Hello Admin"
 
 
-if __name__ == "__main__":​
+if __name__ == "__main__":
     app.run(debug=True)
